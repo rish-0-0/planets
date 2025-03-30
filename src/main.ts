b37d91cdb568c186192de4869c68cad970b93626
Rishabh Anand <rishabhanand42@gmail.com>
2025-03-31 03:58:45 +0530

    messed up

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ import {updateBodyPosition} from "./utils/gravity.ts";
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  window.innerWidth / window.innerHeight,
-  0.1
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
 
 camera.position.z = 30;
